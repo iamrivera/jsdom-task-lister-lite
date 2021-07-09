cb5d4928e91186ebc70ef51bd22ddc3580a937c7
iamrivera <a3m3rivera@gmail.com>
2021-07-08 21:42:51 -0700

    Done.

--- src/index.js
+++ src/index.js
@@ -1,3 +1,16 @@
 document.addEventListener("DOMContentLoaded", () => {
   // your code here
+
+  let buttonSubmit = document.querySelector('#create-task-form').addEventListener('submit', function(event) {
+    event.preventDefault();
+    console.log('Does not redirect');
+    let userValue = document.querySelector('#new-task-description').value;
+    let listItem = document.createElement("li"); 
+    listItem.innerHTML = userValue;
+    document.querySelector('#tasks').append(listItem);
+    document.querySelector('#create-task-form').reset();
+}, false);
+
+
+
 });
